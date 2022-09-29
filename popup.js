eb2896f224673a90b5d41d4e4c2fb830c4d9ed02
Abe Sanchez <abe.sanchez.work@gmail.com>
2022-09-29 23:00:50 +0200

    update

---
+++ popup.js
@@ -0,0 +1,15 @@
+const redBtn = document.getElementById("redButton");
+const blueBtn = document.getElementById("redButton");
+const whiteBtn = document.getElementById("redButton");
+
+redBtn.onclick = () => {
+	console.log(chrome.tabs);
+};
+
+blueBtn.onclick = () => {
+	console.log(chrome.tabs);
+};
+
+whiteBtn.onclick = () => {
+	console.log(chrome.tabs);
+};
